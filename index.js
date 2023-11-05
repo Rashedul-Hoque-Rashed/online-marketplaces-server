e2a62d08360d53bd46af5203f322aa0120705f0e
Rashed <rashedulhoqueceo@gmail.com>
2023-11-06 00:36:32 +0600

    post jobs

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 
 
@@ -24,12 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollections = client.db("jobsDB").collection('jobs');
+    
+    app.post("/api/v1/jobs", async (req, res) => {
+        const newJobs = req.body;
+        const result = await jobsCollections.insertOne(newJobs);
+        res.send(result);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
