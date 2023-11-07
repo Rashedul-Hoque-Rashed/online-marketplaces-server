245464e591b670971ac582fff5c901b2924bc7f7
Rashed <rashedulhoqueceo@gmail.com>
2023-11-08 03:15:05 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173',
+'https://online-marketplace-67d40.web.app'
+],
     credentials: true
 }));
 app.use(express.json());
